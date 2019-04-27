3114350f85eacda60792a6e3b0de4894e7a65a78
armando <armandohernandez0194@gmail.com>
2019-04-27 10:59:10 -0600

    Added login activity

---
+++ app/src/main/java/cs/utep/edu/app10/Activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package cs.utep.edu.app10.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cs.utep.edu.app10.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
